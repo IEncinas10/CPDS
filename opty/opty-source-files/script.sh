2ae1d254d86e39cf4b2eb8eeb6d934d25536002e
IEncinas10 <ignacioencinasrubio@gmail.com>
2022-10-08 21:16:58 +0200

    Script update

--- opty/opty-source-files/script.sh
+++ opty/opty-source-files/script.sh
@@ -1,115 +1,83 @@
 #!/bin/bash
 
-sleep_p1=(200)
-sleep_p2=(200)
-sleep_p3=(200)
+#num_clients=(1 2 3 4)
+num_clients=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23)
+num_entries=(1 11 21 31 41 51 61 71 81 91 101)
+num_reads=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+num_writes=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
 
-#acceptor_delays=(50)
-#acceptor_delays=(50 100 150 200 250 300 350 400 450 500 550 600 650 750)
-#acceptor_delays=(800 850 900 950 1000 1050 1100 1150 1200 1250 1300 1350 1400)
+read_ratios=(0 0.10 0.20 0.30 0.40 0.50 0.60 0.70 0.80 0.90 1)
 
-#acceptor_delays=(50 100 150 200 250 300 350 400 450 500 550 600 650 750 800 850 900 950 1000 1050 1100 1150 1200 1250 1300 1350 1400)
-#acceptor_delays=(1600 1800 2000 2200 2400 2600 2800 3000)
-#acceptor_delays=(2600 2800 3000 3500 4000 4500 5000 5500)
-
-#acceptor_delays=(1000 100 1050 1100 1150 1200 1250 1300 1350 1400 150 1600 1800 2000 200 2200 2400 250 2600 2800 3000 300 3500 350 4000 400 4500 450 5000 500 50  5500 550 600 650 750 800 850 900 950)
-#acceptor_delays=(50 100 150 200 250 300 350 400 450 500 550 600 650 750 800 850 900 950 1000 1050 1100 1150 1200 1250 1300 1350 1400 1600 1800 2000 2200 2400 2600 2800 3000 3500 4000 4500 5000 5500)
-
-acceptor_delays=(1)
-
-drop_probabilities=(5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95)
-
-num_proposers=(1 2 3 4 5 6 7 8)
-num_acceptors=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19)
+total=20
 
 
+
+# perl -e 'print int(x*y)'
+#read_ratio=5; total=100; perl -le 'print '"$read_ratio"'*'"$total"''
+# reads =$(perl -le 'print '"$read_ratio"' * '"$total"' ')
+# writes=$(expr $total - $reads)
+
+#sleeptime=5
+#maxtime=4
+sleeptime=10
+maxtime=8
+
+d_entries=10
+d_clients=2
+d_writes=1
+d_reads=1
 #rm exp1/*
 erl -make
 
-for i in "${sleep_p1[@]}"; do
-    for j in "${sleep_p2[@]}"; do
-	for k in "${sleep_p3[@]}"; do
-	    for d in "${acceptor_delays[@]}"; do
-		#echo $i, $j, $k, $d;
-		#echo "export delay=$d; erl -pa ebin -eval paxy:start["$i", "$j", "$k"] > exp1/paxy"$i"_"$j"_"$k"_"$d".out & pid=$!; sleep 20; kill $pid"
+#for clients in "${num_clients[@]}"; do
+    #echo $clients, $j, $k, $d;
+    #filename=opty"$clients".out;
+    #erl -noshell -pa ebin -eval "opty:start($clients, $d_entries, $d_reads, $d_writes, $maxtime)" > numclients/$filename & pid=$!; sleep $sleeptime; kill $pid
+    #geomean=$(grep "Mean" numclients/$filename | awk -F '[:]' '{print $2}')
+    #stddev=$(grep "Stddev" numclients/$filename | awk -F '[:]' '{print $2}')
+    #echo $clients, $geomean, $stddev >> numclients/clean
+#done
 
-		#filename=paxy"$i"_"$j"_"$k"_"$d".out;
+#for entries in "${num_entries[@]}"; do
+    #echo $entries, $j, $k, $d;
+    #filename=opty"$entries".out;
+    #erl -noshell -pa ebin -eval "opty:start($d_clients, $entries, $d_reads, $d_writes, $maxtime)" > numentries/$filename & pid=$!; sleep $sleeptime; kill $pid
+    #geomean=$(grep "Mean" numentries/$filename | awk -F '[:]' '{print $2}')
+    #stddev=$(grep "Stddev" numentries/$filename | awk -F '[:]' '{print $2}')
+    #echo $entries, $geomean, $stddev >> numentries/clean
+#done
 
-		# RUN, GET TIME TO REACH CONSENSUS
-		#export delay=$d; erl -noshell -pa ebin -eval "paxy:start([$i, $j, $k])" > exp1/$filename & pid=$!; sleep 35; kill $pid
-		#result=$(grep "Total" exp1/$filename | awk -F '[:]' '{print $2}')
-		#echo $d, $result >> exp1/clean
+#for reads in "${num_reads[@]}"; do
+    #echo $reads, $j, $k, $d;
+    #filename=opty"$reads".out;
+    #erl -noshell -pa ebin -eval "opty:start($d_clients, $d_entries, $reads, $d_writes, $maxtime)" > numreads/$filename & pid=$!; sleep $sleeptime; kill $pid
+    #geomean=$(grep "Mean" numreads/$filename | awk -F '[:]' '{print $2}')
+    #stddev=$(grep "Stddev" numreads/$filename | awk -F '[:]' '{print $2}')
+    #echo $reads, $geomean, $stddev >> numreads/clean
+#done
 
-		# NUMBER OF ROUNDS TO REACH AGREEMENT
-		#result=$(grep "DECIDED" exp1/$filename | tail -1)
-		#echo $d, $result >> exp1/rounds
+#for writes in "${num_writes[@]}"; do
+    #echo $writes, $j, $k, $d;
+    #filename=opty"$writes".out;
+    #erl -noshell -pa ebin -eval "opty:start($d_clients, $d_entries, $d_reads, $writes, $maxtime)" > numwrites/$filename & pid=$!; sleep $sleeptime; kill $pid
+    #geomean=$(grep "Mean" numwrites/$filename | awk -F '[:]' '{print $2}')
+    #stddev=$(grep "Stddev" numwrites/$filename | awk -F '[:]' '{print $2}')
+    #echo $writes, $geomean, $stddev >> numwrites/clean
+#done
 
-		 #NUMBER OF TIMEOUTS
-		#filename=paxy"$i"_"$j"_"$k"_"$d".out;
-		#result=$(grep "Timed out" nosorry/$filename | wc -l)
-		#echo $d, $result >> nosorry/timeouts
+# reads =$(perl -le 'print '"$read_ratio"' * '"$total"' ')
+# writes=$(expr $total - $reads)
+for ratio in "${read_ratios[@]}"; do
+    filename=opty"$ratio".out;
+    reads=$(perl -le 'print int('"$ratio"' * '"$total"') ')
+    writes=$(expr $total - $reads)
 
+    echo $ratio, $writes, $reads, $total;
 
-		## RUN, GET TIME TO REACH CONSENSUS
-		#filename=paxy"$i"_"$j"_"$k"_"$d".out;
-		#export delay=$d; erl -noshell -pa ebin -eval "paxy:start([$i, $j, $k])" > nosorry2/$filename & pid=$!; sleep 35; kill $pid
-		#result=$(grep "Total" nosorry2/$filename | awk -F '[:]' '{print $2}')
-		#echo $d, $result >> nosorry2/clean
-
-		for p in "${drop_probabilities[@]}"; do
-		    #echo $i, $j, $k, $d, $p;
-
-
-		    # Drop promises
-		    #filename=paxy"$i"_"$j"_"$k"_"$d"_"$p".out;
-		    #export delay=$d; export drop=$p; erl -noshell -pa ebin -eval "paxy:start([$i, $j, $k])" > droppromise/$filename & pid=$!; sleep 35; kill $pid
-		    #result=$(grep "Total" droppromise/$filename | awk -F '[:]' '{print $2}')
-		    #echo $p, $result >> droppromise/clean
-
-		    # Drop vote
-		    #filename=paxy"$i"_"$j"_"$k"_"$d"_"$p".out;
-		    #export delay=$d; export drop=$p; erl -noshell -pa ebin -eval "paxy:start([$i, $j, $k])" > dropvote/$filename & pid=$!; sleep 35; kill $pid
-		    #result=$(grep "Total" dropvote/$filename | awk -F '[:]' '{print $2}')
-		    #echo $p, $result >> dropvote/clean
-
-		    # Drop both
-		    #filename=paxy"$i"_"$j"_"$k"_"$d"_"$p".out;
-		    #export delay=$d; export drop=$p; erl -noshell -pa ebin -eval "paxy:start([$i, $j, $k])" > dropboth/$filename & pid=$!; sleep 35; kill $pid
-		    #result=$(grep "Total" dropboth/$filename | awk -F '[:]' '{print $2}')
-		    #echo $p, $result >> dropboth/clean
-
-		    # NUMBER OF ROUNDS TO REACH AGREEMENT
-		    filename=paxy"$i"_"$j"_"$k"_"$d"_"$p".out;
-		    result=$(grep "DECIDED" dropboth/$filename | tail -1)
-		    echo $p, $result >> dropboth/rounds
-
-		    result=$(grep "DECIDED" dropvote/$filename | tail -1)
-		    echo $p, $result >> dropvote/rounds
-
-		    result=$(grep "DECIDED" droppromise/$filename | tail -1)
-		    echo $p, $result >> droppromise/rounds
-		done
-
-		#for p in "${num_proposers[@]}"; do
-		    #for a in "${num_acceptors[@]}"; do
-
-			#echo $i, $j, $k, $d, $p, $a;
-			#filename=paxy"$i"_"$j"_"$k"_"$d"_"$p"_"$a".out;
-			#export delay=$d; export proposers=$p; export acceptors=$a; erl -noshell -pa ebin -eval "paxy:start([$i, $j, $k, $k, $k, $k, $k, $k])" > exp4/$filename & pid=$!; sleep 15; kill $pid
-			#result=$(grep "Total" exp4/$filename | awk -F '[:]' '{print $2}')
-			#echo $p, $a, $result >> exp4/clean
-		    #done
-		#done
-
-		## RUN, GET TIME TO REACH CONSENSUS
-		#filename=paxy"$i"_"$j"_"$k"_"$d".out;
-		#export delay=$d; erl -noshell -pa ebin -eval "paxy:start([$i, $j, $k])" > improvement/$filename & pid=$!; sleep 35; kill $pid
-		#result=$(grep "Total" improvement/$filename | awk -F '[:]' '{print $2}')
-		#echo $d, $result >> improvement/clean
-	    done
-
-	done
-    done
+    erl -noshell -pa ebin -eval "opty:start($d_clients, $d_entries, $reads, $writes, $maxtime)" > readratio/$filename & pid=$!; sleep $sleeptime; kill $pid
+    geomean=$(grep "Mean" readratio/$filename | awk -F '[:]' '{print $2}')
+    stddev=$(grep "Stddev" readratio/$filename | awk -F '[:]' '{print $2}')
+    echo $ratio, $geomean, $stddev >> readratio/clean
 done
 
 #sort -V -t ',' -k1 oldrounds > rounds
