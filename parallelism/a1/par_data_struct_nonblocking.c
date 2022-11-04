a3eadcbaaab12737d5c55291a93c3b80395b895a
IEncinas10 <ignacioencinasrubio@gmail.com>
2022-11-04 12:23:56 +0100

    Fill par_data_struct_nonblocking

--- parallelism/a1/par_data_struct_nonblocking.c
+++ parallelism/a1/par_data_struct_nonblocking.c
@@ -1,6 +1,6 @@
+#include "mpi.h"
 #include <stdio.h>
 #include <unistd.h>
-#include "mpi.h"
 
 /* This code uses NONBLOCKING COMMUNICATIONS and illustrates MPI_Waitall */
 
@@ -13,7 +13,7 @@
 #define maxn 12
 #define P 4
 
-int main( argc, argv )
+int main(argc, argv)
 int argc;
 char **argv;
 {
@@ -21,36 +21,35 @@ char **argv;
     MPI_Request r[4];
     int nreq;
     MPI_Status statuses[4];
-    //double x[maxn][maxn];
-    double xlocal[(maxn/P)+2][maxn];
+    // double x[maxn][maxn];
+    double xlocal[(maxn / P) + 2][maxn];
 
-    MPI_Init( &argc, &argv );
+    MPI_Init(&argc, &argv);
 
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &size );
-
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     if (size != P) {
-       printf("This program only works when run with %d processes. Aborting.\n", P);
-       usleep(10);
-       MPI_Abort( MPI_COMM_WORLD, 1 );
+	printf("This program only works when run with %d processes. Aborting.\n", P);
+	usleep(10);
+	MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
     /* xlocal[0][] refers to the initial row. It's used to store ghost points
-                   used to keep data coming from the previous process */
+		   used to keep data coming from the previous process */
 
     /* xlocal[maxn/size+1][] refers to the final row: it's used to store ghost
-                   points used to keep data coming from the next process */
+		   points used to keep data coming from the next process */
 
     /* Fill the data as specified */
     /* 1) Fill its local segment with the rank of the process */
-    for (i=1; i<=maxn/size; i++) 
-	for (j=0; j<maxn; j++) 
+    for (i = 1; i <= maxn / size; i++)
+	for (j = 0; j < maxn; j++)
 	    xlocal[i][j] = rank;
     /* 2) Fill ghost points with -1 */
-    for (j=0; j<maxn; j++) {
+    for (j = 0; j < maxn; j++) {
 	xlocal[0][j] = -1;
-	xlocal[maxn/size+1][j] = -1;
+	xlocal[maxn / size + 1][j] = -1;
     }
 
     /* We use xlocal[i] for &xlocal[i][0] to provide the initial address
@@ -59,52 +58,64 @@ char **argv;
     /* Send last local row to next process (to process with identifier rank+1)
        unless I'm the last one (process with identifier size-1). */
     nreq = 0;
-    if (rank < size - 1) 
-	MPI_Isend(         ,     , MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD,
-                  &r[nreq++] ); /* Statement S7 */
+    if (rank < size - 1)
+	MPI_Isend(xlocal[maxn / size], maxn, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &r[nreq++]); /* Statement S7 */
+    /* S7: specify sendbuff and number of elements to send */
+
     /* Receive from previous unless I'm the first process (rank==0)
        and store row in initial row (ghost area) */
     if (rank > 0)
-	MPI_Irecv(         ,     , MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, 
-		  &r[nreq++] ); /* Statement S8 */
+	MPI_Irecv(xlocal, maxn, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &r[nreq++]); /* Statement S8 */
+    /* S8: specify recvbuff and number of elements to receive */
 
     /* Send 1st local row to previous process unless I'm the 1st process */
-    if (rank > 0) 
-	MPI_Isend(         ,     , MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD,
-		  &r[nreq++] ); /* Statement S9 */
+    if (rank > 0)
+	MPI_Isend(xlocal[1], maxn, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD, &r[nreq++]); /* Statement S9 */
+    /* S9: specify sendbuff(first owned row) and number of elements to send */
+
     /* Receive from next process and store row in last row in xlocal
        (ghost area) unless I'm the last process */
-    if (rank < size - 1) 
-	MPI_Irecv(         ,     , MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD,
-                             ); /* Statement S10 */
+    if (rank < size - 1)
+	MPI_Irecv(xlocal[maxn / size + 1], maxn, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD,
+		  &r[nreq++]); /* Statement S10 */
+    /*
+     * S10: specify recv(last owned row) and number of elements to send. Also, specify which element of the MPI_Request
+     * r array must hold the communication information.
+     *
+     * The nreq++ here is important because we'll use it in MPI_Waitall. As we know the information beforehand
+     * I would use its known size instead of relying on this '++', it seems kind of error prone.
+     */
 
     /* Wait for all four requests with a single call */
-    MPI_Waitall( nreq, r, statuses );
+    MPI_Waitall(nreq, r, statuses);
 
     /* Check that we have the correct results after the communications */
     errcnt = 0;
-    /* Check that we have the correct results in the internal part 
+    /* Check that we have the correct results in the internal part
        (They have not been overwritten due to some error). */
-    for (i=1; i<=maxn/size; i++) 
-	for (j=0; j<maxn; j++) 
-	    if (xlocal[i][j] != rank) errcnt++;
+    for (i = 1; i <= maxn / size; i++)
+	for (j = 0; j < maxn; j++)
+	    if (xlocal[i][j] != rank)
+		errcnt++;
     /* Check that we have the correct results in the ghost areas */
-    for (j=0; j<maxn; j++) {
-	if (xlocal[0][j] != rank - 1) errcnt++;
-	if (rank < size-1 && xlocal[maxn/size+1][j] != rank + 1) errcnt++;
+    for (j = 0; j < maxn; j++) {
+	if (xlocal[0][j] != rank - 1)
+	    errcnt++;
+	if (rank < size - 1 && xlocal[maxn / size + 1][j] != rank + 1)
+	    errcnt++;
     }
 
     /* Retrieve in process 0 the total number of errors */
-    MPI_Reduce(    ,    , 1, MPI_INT, MPI_SUM, 0,
-               MPI_COMM_WORLD); /* Statement S11 */
+    MPI_Reduce(&errcnt, &toterr, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD); /* Statement S11 */
+    /* S11: specify sendbuff(local error) and recvbuff(total error) for the reduce. */
 
     if (rank == 0) {
 	if (toterr)
-	    printf( "ERROR: found %d errors\n", toterr );
+	    printf("ERROR: found %d errors\n", toterr);
 	else
-	    printf( "No errors\n" );
+	    printf("No errors\n");
     }
 
-    MPI_Finalize( );
+    MPI_Finalize();
     return 0;
 }
