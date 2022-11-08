6a8b3ae3aee22b75e2de4f24e61adf15f65b2d55
Adrian <adrianjg@hotmail.es>
2022-11-08 13:15:21 +0100

    f

--- parallelism/a2/part3/heat-mpi.c
+++ parallelism/a2/part3/heat-mpi.c
@@ -110,14 +110,15 @@ int main( int argc, char *argv[] )
         while(1) {
         switch( param.algorithm ) {
             case 0: // JACOBI
-                    //MPI_Send(&param.u[np*(rowsWorkers-1)], np, MPI_DOUBLE, myid+1, 0, MPI_COMM_WORLD);
-                    //MPI_Recv(&param.u[np*rowsWorkers], np, MPI_DOUBLE, myid+1, 0,  MPI_COMM_WORLD, &status);
+                    MPI_Send(&param.u[np*rowsWorkers], np, MPI_DOUBLE, myid+1, 0, MPI_COMM_WORLD);
+                    MPI_Recv(&param.u[np*rowsWorkers+1], np, MPI_DOUBLE, myid+1, 0,  MPI_COMM_WORLD, &status);
                     //printf("MAGIC\n");
                     residual = relax_jacobi(param.u, param.uhelp, np, rowsWorkers);
                 // Copy uhelp into u
-                for (int i=0; i<rowsWorkers+2; i++)
-                        for (int j=0; j<np; j++)
-                        param.u[ i*np+j ] = param.uhelp[ i*np+j ];
+                    double *aux = param.u;
+                    param.u = param.uhelp;
+                    param.uhelp = aux;
+                        
                 break;
             case 1: // RED-BLACK
                 residual = relax_redblack(param.u, np, np);
@@ -130,16 +131,16 @@ int main( int argc, char *argv[] )
             iter++;
 
             // solution good enough ?
-            if (residual < 0.00005) break;
+            //if (residual < 0.00005) break;
 
             // max. iteration reached ? (no limit with maxiter=0)
-            if (param.maxiter>0 && iter>=param.maxiter) break; 
+            if (/*param.maxiter>0 && */iter>=param.maxiter) break; 
         }
 
         //receive image from workers
-
+        printf("Receiving...");
         for(int i = 1; i < numprocs; i++){
-            MPI_Recv(&param.u[np*rowsWorkers*i], np*(rowsWorkers+2), MPI_DOUBLE, i, i, MPI_COMM_WORLD, &status);
+            MPI_Recv(&param.u[np*rowsWorkers*i+1], np*(rowsWorkers), MPI_DOUBLE, i, i, MPI_COMM_WORLD, &status);
         }
 
         // Flop count after iter iterations
@@ -204,23 +205,39 @@ int main( int argc, char *argv[] )
         printf("uhelp and u received \n");
         printf("Values rows = %d, columns = %d \n", rows, columns);
 
+        if(myid == numprocs-1){
+            for(int i = 0; i < rows+2; i++){
+                for(int j =0; j< columns+2; j++){
+                    printf("%1.1f ", u[i*(columns+2)+j]);
+                }
+                printf("\n");
+            }
+        }
+
         iter = 0;
         while(1) {
         switch( algorithm ) {
             case 0: // JACOBI
-                    //MPI_Recv(&u[0], columns + 2, MPI_DOUBLE, myid-1, 0, MPI_COMM_WORLD, &status);
-                    //MPI_Send(&u[columns + 2], columns + 2, MPI_DOUBLE, myid-1, 0, MPI_COMM_WORLD);
-                    //printf("alo");
-                    //if(myid != numprocs - 1){
-                        //MPI_Send(&u[rows*(columns+2)], columns+2, MPI_DOUBLE, myid+1, 0, MPI_COMM_WORLD);
-                        //MPI_Recv(&u[(rows+1*(columns+2)) * (columns + 2)], columns+2, MPI_DOUBLE, myid+1, 0, MPI_COMM_WORLD, &status);
-                    //}
+                    MPI_Send(&u[columns+2], columns + 2, MPI_DOUBLE, myid-1, 0, MPI_COMM_WORLD);
 
-                    residual = relax_jacobi(u, uhelp, np, rows + 2);
+                    MPI_Recv(&u[0], columns + 2, MPI_DOUBLE, myid-1, 0, MPI_COMM_WORLD, &status);
+                    
+                    //printf("alo");
+                    if(myid != numprocs - 1){
+                        MPI_Send(&u[rows*(columns+2)], columns+2, MPI_DOUBLE, myid+1, 0, MPI_COMM_WORLD);
+                        MPI_Recv(&u[(rows+1)*(columns + 2)], columns+2, MPI_DOUBLE, myid+1, 0, MPI_COMM_WORLD, &status);
+                    }
+                    if(myid == 1)
+                        printf("%d\n", iter);
+
+                    residual = relax_jacobi(u, uhelp, np, rows+2);
                 // Copy uhelp into u
-                for (int i=0; i<rows+2; i++)
-                        for (int j=0; j<np; j++)
-                        u[ i*np+j ] = uhelp[ i*np+j ];
+                /*for (int i=1; i<rows+1; i++)
+                        for (int j=1; j<columns+1; j++)
+                        u[ i*np+j ] = uhelp[ i*np+j ];*/
+                    double *aux = u;//cambiado para evitar porblemas con los indices...
+                    u = uhelp;
+                    uhelp = aux;
                 break;
             case 1: // RED-BLACK
                 residual = relax_redblack(u, np, np);
@@ -233,13 +250,13 @@ int main( int argc, char *argv[] )
             iter++;
 
             // solution good enough ?
-            if (residual < 0.00005) break;
+            //if (residual < 0.00005) break;
 
             // max. iteration reached ? (no limit with maxiter=0)
-            if (maxiter>0 && iter>=maxiter) break;
+            if (/*maxiter>0 &&*/ iter>=maxiter) break;
         }
-
-        MPI_Send(&u[0], (columns + 2)*(rows + 2), MPI_DOUBLE, 0, myid, MPI_COMM_WORLD);
+        printf("ENVIANDO");
+        MPI_Send(&u[columns+2], (columns + 2)*(rows), MPI_DOUBLE, 0, myid, MPI_COMM_WORLD);
 
         if( u ) free(u); 
         if( uhelp ) free(uhelp);
